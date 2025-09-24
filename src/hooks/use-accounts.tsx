217e2802a16f182edeb8215bd2c240e11fc0cfdb
Clemence Goh <clemencegoh@gmail.com>
2025-09-24 19:29:48 +0800

    temp save

--- src/hooks/use-accounts.tsx
+++ src/hooks/use-accounts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Account, NewAccount } from "@/lib/types";
 import { useToast } from "@/hooks/use-toast";
 import { useState } from "react";
